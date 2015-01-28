a0ba51166d5dcadf0cfe79a142096dcb9462a5bd
Jorge Antunes <ask@stoikerty.com>
2015-01-28 15:01:06 +0000

    update toolbox with latest scss helpers, enabling minification, adding scss animation library & config files

--- config.rb
+++ config.rb
@@ -12,8 +12,6 @@ require 'slim'
 Slim::Engine.disable_option_validator!
 
 page "/index.html"
-page "/responsive.html"
-page "/multi.html"
 
 # ---
 
@@ -73,10 +71,10 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
