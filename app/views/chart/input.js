fbe45926405ad8ad63d2f7174c99ce78c1f22fcf
Jorge Antunes <ask@stoikerty.com>
2015-01-30 11:05:18 +0000

    add more sample data

--- app/views/chart/input.js
+++ app/views/chart/input.js
@@ -11,9 +11,7 @@ dataSetCollection.add({
     id : '0',
     title : '5 inch gloss'
 });
-
 dataSetCollection.at(0).series = new SeriesCollection();
-
 dataSetCollection.at(0).series.add({ id : '0', label : 'Orders', value : 50 });
 dataSetCollection.at(0).series.add({ id : '1', label : 'Items', value : 700 });
 
@@ -21,11 +19,25 @@ dataSetCollection.add({
     id : '1',
     title : '5 inch lustre'
 });
-
 dataSetCollection.at(1).series = new SeriesCollection();
-
 dataSetCollection.at(1).series.add({ id : '0', label : 'Orders', value : 30 });
 dataSetCollection.at(1).series.add({ id : '1', label : 'Items', value : 500 });
+
+dataSetCollection.add({
+    id : '2',
+    title : '6 inch gloss'
+});
+dataSetCollection.at(2).series = new SeriesCollection();
+dataSetCollection.at(2).series.add({ id : '0', label : 'Orders', value : 40 });
+dataSetCollection.at(2).series.add({ id : '1', label : 'Items', value : 600 });
+
+dataSetCollection.add({
+    id : '3',
+    title : '6 inch lustre'
+});
+dataSetCollection.at(3).series = new SeriesCollection();
+dataSetCollection.at(3).series.add({ id : '0', label : 'Orders', value : 60 });
+dataSetCollection.at(3).series.add({ id : '1', label : 'Items', value : 900 });
 
 
 
