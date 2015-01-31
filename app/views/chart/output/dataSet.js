477ba9d3ffbc7902847d4cf1dfd322c4bad42c4f
Jorge Antunes <ask@stoikerty.com>
2015-01-31 14:37:31 +0000

    show scale numbers visually, updating them when series_item values have changed

--- app/views/chart/output/dataSet.js
+++ app/views/chart/output/dataSet.js
@@ -13,15 +13,35 @@ module.exports = View.extend({
         },
         'model.maxDataSetValue': {
             type: function (el, value, previousValue) {
-                // update all items to use the correct height
-                // by triggering a change on their model
-                this.model.dataSet.forEach(function(model){
-                    model.series.forEach(function(series_item){
-                        series_item.trigger('change');
-                    });
-                });                
+                this.updateSeriesItems();
+                this.updateScaleNumbers();    
             },
             hook: 'value'
+        }
+    },
+
+    round : function(x, digits){
+        return parseFloat(x.toFixed(digits));
+    },
+
+    // update all items to use the correct height
+    // by triggering a change on their model
+    updateSeriesItems : function(){
+        this.model.dataSet.forEach(function(model){
+            model.series.forEach(function(series_item){
+                series_item.trigger('change');
+            });
+        });
+    },
+
+    // make the text in the numbers scale reflect the item values
+    updateScaleNumbers : function(){
+        for (i = 0; i < this.allNumberElements.length; i++){
+            var selected = this.allNumberElements.length - (i + 1);
+            var maxNumber = this.model.maxDataSetValue;
+            var percentage = ((i+1) / (this.allNumberElements.length));
+
+            this.allNumberElements[selected].innerText = this.round((maxNumber * percentage), 0);
         }
     },
 
@@ -29,6 +49,10 @@ module.exports = View.extend({
     },
     render : function(options){
         this.renderWithTemplate();
+
+        // cache scale number elements
+        this.allNumberElements = this.queryAll('[data-hook="scale-number"]');
+        this.updateScaleNumbers();
 
         this.renderCollection(
             this.model.dataSet,
